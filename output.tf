910eb9935036cd9757c53da4daad550a3858d426
happy111 <umeshsamal3@gmail.com>
2024-10-23 08:43:50 +0530

    Update code

---
+++ output.tf
@@ -0,0 +1,16 @@
+output "cluster_id" {
+  value = aws_eks_cluster.love.id
+}
+
+output "node_group_id" {
+  value = aws_eks_node_group.love.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.love_vpc.id
+}
+
+output "subnet_ids" {
+  value = aws_subnet.love_subnet[*].id
+}
+
